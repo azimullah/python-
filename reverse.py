7e079be409e6b38cd065f13deb76a952ecab8eb0
azimullah <mirza.mnnit@gmail.com>
2025-05-20 10:38:12 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,7 @@
+string= input ("please enter your own string  " )
+string2= ('')
+for i in string :
+    string2 = i + string2
+
+    print("\nThe Orignal string =", string)
+    print("\nThe Reversed string =", string2)
