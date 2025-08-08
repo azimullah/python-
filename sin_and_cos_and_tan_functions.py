f35452d2afb706ed92c935fb847a0c31ec0d7b41
azimullah <mirza.mnnit@gmail.com>
2025-08-08 15:27:04 +0530

    Add files via upload

---
+++ sin_and_cos_and_tan_functions.py
@@ -0,0 +1,8 @@
+import math as m
+
+input_value = float(input("Enter a number you want to make to cos: "))
+input_value = float(input("Enter a number you want to make to sin: "))
+input_value = float(input("Enter a number you want to make to tan: "))
+print(input_value)
+print(m.sin(input_value))
+print(m.tan(input_value))
---
+++ square_turtle.py
@@ -0,0 +1,10 @@
+import turtle
+
+bob = turtle.Turtle()  # Capital "T" in Turtle
+
+bob.forward(100)  # Move forward by 100 units
+bob.left(90)      # Turn left by 90 degrees
+bob.forward(100)  # Move forward by another 100 units
+bob.left(90)      # Turn left by 90 degrees again
+bob.forward(100)  # Move forward by 100 units again
+bob.left(90)      # Turn left by 90 degrees again
