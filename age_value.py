3589f0495129e2285705f8fa99f7961b882192be
azimullah <mirza.mnnit@gmail.com>
2025-08-09 10:44:59 +0530

    Add files via upload

---
+++ age_value.py
@@ -0,0 +1,11 @@
+age_value= int(input(" please enter an age above  20: "))
+if age_value < 20:
+    print("Age must be above 20.")
+else:
+    print("you can enter.")  
+if age_value % 2 == 0:
+    print(f"{age_value} is even")
+else:
+    print(f"{age_value} is odd")
+# Output: For each number from 0 to input_range-1, prints the double of the number and whether it is even or odd.
+
